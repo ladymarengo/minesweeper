019d9810202a7c415e075af85435830e2277f405
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-06-15 16:39:10 +0300

    Change nested ifs to throws

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include <iostream>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -14,49 +15,35 @@ void close();
 
 bool init()
 {
-	bool success = true;
+    try
+    {
+        if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+            throw "SDL could not initialize!";
+        
+        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+            throw "Warning: Linear texture filtering not enabled!";
 
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+            throw "Window could not be created!";
 
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+        if( gRenderer == NULL )
+            throw "Renderer could not be created!";
 
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-	return success;
+        int imgFlags = IMG_INIT_PNG;
+        if( !( IMG_Init( imgFlags ) & imgFlags ) )
+            throw "SDL_image could not initialize!";
+    }
+    catch (const char* exception)
+    {
+        std::cerr << "Error: " << exception << '\n';
+        return false;
+    }
+
+	return true;
 }
 
 void close()
@@ -72,31 +59,35 @@ void close()
 
 int main( int argc, char* args[] )
 {
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{	
-        bool quit = false;
-        SDL_Event e;
+    try
+    {
+        if( !init() )
+            throw "Failed to initialize!\n";
+    }
+    catch (const char* exception)
+    {
+        std::cerr << "Error: " << exception << '\n';
+        close();
+        return 0;
+    }
+	bool quit = false;
+    SDL_Event e;
 
-        while( !quit )
+    while( !quit )
+    {
+        while( SDL_PollEvent( &e ) != 0 )
         {
-            while( SDL_PollEvent( &e ) != 0 )
+            if( e.type == SDL_QUIT )
             {
-                if( e.type == SDL_QUIT )
-                {
-                    quit = true;
-                }
+                quit = true;
             }
-
-            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
-            SDL_RenderClear( gRenderer );
-
-            SDL_RenderPresent( gRenderer );
         }
-	}
+
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
+        SDL_RenderClear( gRenderer );
+
+        SDL_RenderPresent( gRenderer );
+    }
 	close();
 	return 0;
 }
